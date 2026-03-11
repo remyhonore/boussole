6980455d5106e18fe521403b5909d1863a862db4
Remy Honore <remyhonore@protonmail.com>
2026-03-11 18:55:54 +0100

    feat: cache busting v4.2 (query string + SW bump)

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Cache offline : app utilisable sans connexion après premier chargement
  */
 
-const CACHE_NAME = 'boussole-v4.1';
+const CACHE_NAME = 'boussole-v4.2';
 
 const ASSETS_TO_CACHE = [
   '/',
