3846a9146af05e0b5dbce6cd34e19a440652e867
Remy Honore <remyhonore@protonmail.com>
2026-04-04 13:36:00 +0200

    style: design tokens v2 + classes utilitaires + normalisation inline styles (audit UX)

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Cache offline : app utilisable sans connexion après premier chargement
  */
 
-const CACHE_NAME = 'boussole-v9.37';
+const CACHE_NAME = 'boussole-v9.38';
 
 const ASSETS_TO_CACHE = [
   '/app.js',
